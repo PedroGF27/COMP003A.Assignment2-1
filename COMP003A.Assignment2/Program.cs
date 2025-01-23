22032f48b28dae474b8d3819732ea542be1f4af5
PedroGF27 <coolcatdopedog@gmail.com>
2025-01-22 18:43:01 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Author: Pedro Garcia
+Course: COMP003A
+Faculty: Jonathan Cruz
+Purpose: COMP-003A - Lecture Actitvity
+*/
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            int age;
+            double price;
+            bool isStudent;
+
+
+            Console.Write("Welcome to the Discount Calculator!");
+            
+
+            Console.Write("What is your first name?: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("What is your last name?: ");
+            lastName = Console.ReadLine();
+            Console.Write("How old are you?: ");
+
+            Console.Write("What is the price of the item you're interested in?: ");
+
+            Console.Write("Are you a student? (true/false)");
+
+        }
+    }
+}
